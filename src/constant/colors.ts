14f9ba1bd4c543826dca3f39f66d3ef731536578
sami-chopdawg <sami@chopdawg.com>
2023-11-28 16:46:30 +0500

    feat:colors updated

---
+++ src/constant/colors.ts
@@ -0,0 +1,34 @@
+const COLORS = {
+  primary: {
+    main: '#494746',
+    light: '#313137',
+  },
+  secondary: {
+    main: '#FF5655',
+    light: '#F23D4C',
+  },
+  info: {
+    main: '#2378EF',
+    light: '#64b5f6',
+  },
+  grey: {
+    main: '#8A8887',
+    light: '#85848B',
+    dark: '#F6F6F6',
+  },
+  input: {
+    main: '#808080',
+    light: '#9D9DAA',
+  },
+  error: {
+    main: '#FF5655',
+  },
+  wine: {
+    main: '#3E173A',
+    dark: '#4E1B5c',
+  },
+  success: '#24CB99',
+  active: '#EEFCF8',
+}
+
+export default COLORS
