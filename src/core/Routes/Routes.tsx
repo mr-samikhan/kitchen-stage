2e4656e70da1746e13e48ec14bdeff82ddf6dfe5
sami-chopdawg <sami@chopdawg.com>
2023-11-28 17:37:32 +0500

    feat:added new screens modules and routes

--- src/core/Routes/Routes.tsx
+++ src/core/Routes/Routes.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Box, Typography } from '@mui/material';
 import { Route, Routes as ReactRoutes } from "react-router-dom";
 
 import { ROUTES } from '@muc/constant';
+import { Dashboard, Login } from '@muc/screens';
 import { ProtectedRoute } from './components/components';
 
 const Routes = () => {
@@ -10,14 +10,12 @@ const Routes = () => {
   return (
   <React.Fragment>
    <ReactRoutes>
-    <Route path={ROUTES.LOGIN_ACCOUNT} element={<h2>Login</h2>} />
+    <Route path={ROUTES.LOGIN_ACCOUNT} element={<Login />} />
    </ReactRoutes>
 
    <ProtectedRoute isAuthenticated={false}>
     <ReactRoutes>
-    <Route path={ROUTES.DASHBOARD}  element={<Box bgcolor="primary.main">
-      <Typography>HOME</Typography>
-    </Box>} />
+    <Route path={ROUTES.DASHBOARD}  element={<Dashboard />} />
     </ReactRoutes>
    </ProtectedRoute>
    
--- src/screens/screens.ts
+++ src/screens/screens.ts
@@ -1,1 +1,3 @@
+export { default as Login } from './Login/Login'
 export { default as NotFound } from './NotFound/NotFound'
+export { default as Dashboard } from './Dashboard/Dashboard'
