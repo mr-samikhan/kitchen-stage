67bb398f99182f71852c39fcd950d204b6ec0661
sami-chopdawg <sami@chopdawg.com>
2023-12-01 22:17:46 +0500

    feat:updated colors palette and theme

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,3 +1,2 @@
-export { default as Test } from './Test/Test'
 export { default as Form } from './Form/Form'
 export { default as CustomTextField } from './CustomTextField/CustomTextField'
--- src/constant/routes.ts
+++ src/constant/routes.ts
@@ -3,6 +3,7 @@ const Routes = {
   USERS: '/users',
   LOGIN_ACCOUNT: '/login',
   DASHBOARD: '/dashboard',
+  FORGOT_PASSWORD: '/forgot_password',
 }
 
 export default Routes
