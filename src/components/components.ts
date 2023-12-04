23105683a4aeac7e78e44d570ef423ee57082dc3
sami-chopdawg <sami@chopdawg.com>
2023-12-04 18:49:15 +0500

    feat:Added dialog box for modals

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,2 +1,3 @@
 export { default as Form } from './Form/Form'
+export { default as CustomDialog } from './CustomDialog/CustomDialog'
 export { default as CustomTextField } from './CustomTextField/CustomTextField'
