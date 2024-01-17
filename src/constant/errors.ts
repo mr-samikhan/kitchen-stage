474a4716a8312051724c94dec5b54573f0fc4012
sami-chopdawg <sami@chopdawg.com>
2024-01-17 17:03:11 +0500

    feat:worked on auth-functionality

---
+++ src/constant/errors.ts
@@ -0,0 +1,33 @@
+const getErrorMessage = (error: any) => {
+  error = error.code || error
+  let errorMsg = ''
+  if (error === 'auth/user-not-found') {
+    errorMsg = 'This email is not registered!'
+  } else if (error === 'auth/user-disabled') {
+    errorMsg = 'Your account has been disabled by admin!'
+  } else if (error === 'auth/invalid-email') {
+    errorMsg = 'That email address is invalid!'
+  } else if (error === 'auth/wrong-password') {
+    errorMsg = 'Invalid Password'
+  } else if (error === 'auth/too-many-requests') {
+    errorMsg = 'Access to this account has been temporarily disabled!'
+  } else if (error === 'auth/network-request-failed') {
+    errorMsg = 'Please check your internet connection!'
+  } else if (error === 'auth/internal-error') {
+    errorMsg = 'An internal error has occurred, please try again!'
+  } else if (error === 'auth/email-already-in-use') {
+    errorMsg = 'Email is already in use!'
+  } else if (error === 'auth/invalid-credential') {
+    errorMsg =
+      'Your email address and password did not match, Please check your credentials and try again!'
+  } else if (error === 'permission-error') {
+    errorMsg = "You don't have permission to do this operation!"
+  } else if (error === 'auth/network-request-failed') {
+    errorMsg = 'You are offline!'
+  } else if (error === 'permission-error') {
+    errorMsg = 'You don`t have permission to delete Adminsitrator!'
+  }
+  return errorMsg
+}
+
+export default getErrorMessage
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import App from './core/App/App.tsx'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ReduxProvider } from '@cookup/providers'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ReduxProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    ,
+  </ReduxProvider>
 )
